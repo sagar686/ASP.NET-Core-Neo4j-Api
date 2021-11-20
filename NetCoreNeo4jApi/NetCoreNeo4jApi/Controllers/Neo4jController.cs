2f11ae9bdf2d9e9b0d8a69c08a6a528c8fd0b7df
Sagar Sangvekar <sag61086@gmail.com>
2021-11-20 21:38:23 +0530

    Initial commit

---
+++ NetCoreNeo4jApi/NetCoreNeo4jApi/Controllers/Neo4jController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+using System.Text;
+
+namespace NetCoreNeo4jApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Neo4jController : ControllerBase
+    {
+        private readonly IDriver _driver;
+
+        public Neo4jController()
+        {
+            _driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "person"));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNode(string name)
+        {
+            var statementText = new StringBuilder();
+            statementText.Append("CREATE (person:Person {name: $name})");
+            var statementParameters = new Dictionary<string, object>() { { "name", name } };
+        
+
+            var session = this._driver.AsyncSession();
+            var result = await session.WriteTransactionAsync(tx => tx.RunAsync(statementText.ToString(), statementParameters));
+            return StatusCode(201, "Node has been created in the database");
+        }
+
+
+    }
+}
